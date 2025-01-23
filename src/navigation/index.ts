7de2ca3163cb7ca4623a149c44e94eabcd1c5254
rosevilleage <chanhee.frontend@gmail.com>
2025-01-23 20:24:52 +0900

    navigation 세팅

---
+++ src/navigation/index.ts
@@ -0,0 +1,11 @@
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import MainScreen from '../screens/MainScreen';
+import {createStaticNavigation} from '@react-navigation/native';
+
+const MainDrawer = createDrawerNavigator({
+  screens: {
+    Main: {screen: MainScreen, options: {headerTitle: ''}},
+  },
+  initialRouteName: 'Main',
+});
+export default createStaticNavigation(MainDrawer);
