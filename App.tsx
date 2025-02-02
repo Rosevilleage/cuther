d31ebd68401714c55d5eb29a65654fd1b709844b
rosevilleage <chanhee.frontend@gmail.com>
2025-02-02 21:12:30 +0900

    android 위치 권한 추가

--- App.tsx
+++ App.tsx
@@ -7,13 +7,17 @@
 import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 import Navigation from './src/navigation';
-import {Platform} from 'react-native';
+import {PermissionsAndroid, Platform} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 
 function App(): React.JSX.Element {
   useEffect(() => {
     if (Platform.OS === 'ios') {
       Geolocation.requestAuthorization('always');
+    } else if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
     }
   }, []);
   return <Navigation />;
