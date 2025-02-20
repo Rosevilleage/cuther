8dc9f8b2219bb70dea1e97e6ad5b43f63dae08ec
rosevilleage <chanhee.frontend@gmail.com>
2025-02-20 18:54:20 +0900

    weather api 수정

--- src/features/weather/api/WeatherApi.ts
+++ src/features/weather/api/WeatherApi.ts
@@ -1,47 +1,62 @@
 import axios from 'axios';
+import {WeatherDTO, WeatherNcstItemDTO} from '../model/weatherDTO';
 
 const weatherFetcher = axios.create({
   baseURL: process.env.API_END_POINT as string,
-  timeout: 5000,
+  timeout: 10000,
 });
 
 weatherFetcher.interceptors.request.use(req => {
-  console.log('wetherReq :', req);
+  // console.log('wetherReq :', req);
   return req;
 });
 
 weatherFetcher.interceptors.response.use(res => {
-  console.log('wetherRes :', res.status);
+  // console.log('wetherRes :', res);
   return res;
 });
 
-export class WeatherApi {
-  private api = weatherFetcher;
+interface WeatherApiProps {
+  nx: number;
+  ny: number;
+  base_date: string;
+  base_time: string;
+}
+export function getNcstWeather({
+  nx,
+  ny,
+  base_date,
+  base_time,
+}: WeatherApiProps) {
+  const params = {
+    serviceKey: process.env.API_KEY as string,
+    numOfRows: 5000,
+    pageNo: 1,
+    dataType: 'JSON',
+    base_date,
+    base_time,
+    nx,
+    ny,
+  };
+  return weatherFetcher.get<WeatherDTO<WeatherNcstItemDTO>>(
+    '/getUltraSrtNcst',
+    {params},
+  );
+}
 
-  async fetchWeather(
-    endPoint: string,
-    {
-      nx,
-      ny,
-      base_date,
-      base_time,
-    }: {
-      nx: number;
-      ny: number;
-      base_date: string;
-      base_time: string;
-    },
-  ) {
-    const params = {
-      serviceKey: process.env.API_KEY as string,
-      numOfRows: 1000,
-      pageNo: 1,
-      dataType: 'JSON',
-      base_date,
-      base_time,
-      nx,
-      ny,
-    };
-    return this.api.get(endPoint, {params});
-  }
+export function getFcstWeather<T>(
+  url: string,
+  {base_date, base_time, nx, ny}: WeatherApiProps,
+) {
+  const params = {
+    serviceKey: process.env.API_KEY as string,
+    numOfRows: 1000,
+    pageNo: 1,
+    dataType: 'JSON',
+    base_date,
+    base_time,
+    nx,
+    ny,
+  };
+  return weatherFetcher.get<WeatherDTO<T>>(url, {params});
 }
