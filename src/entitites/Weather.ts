3d18cb8d738501b07eac6879896ee80113fd6bb2
rosevilleage <chanhee.frontend@gmail.com>
2025-02-20 18:51:54 +0900

    weather entity 수정

--- src/entitites/Weather.ts
+++ src/entitites/Weather.ts
@@ -1,26 +1,38 @@
 export class Weather {
   constructor(
-    public temperature: number,
-    public condition: string,
-    public icon: number,
-    public perceivedTemperature: number,
-    public min: number,
-    public max: number,
+    public temperature: number, // 기온
+    public condition: number, // 날씨 상태
+    public perceivedTemperature: number, // 체감기온
+    public min: number, // 1일 최저 기온
+    public max: number, // 1일 최대 기온
+    public windDirection: string, // 풍향
+    public windSpeed: number, // 풍속
+    public rainWeight: number, // 강수량
+    public humidity: number, // 습도
+    public rain: number, // 강수형태
+    public rainPercent: number, // 강수 확률
+    public snowWeitgh: number, // 석절량
   ) {}
 }
 
-export class HourlyWeather {
+export class CurWeather {
   constructor(
-    public time: string,
-    public temperature: number,
-    public condition: string,
-    public icon: string,
+    public temperature: number, // 기온
+    public condition: number, // 날씨 상태
+    public perceivedTemperature: number, // 체감기온
+    public rainWeight: number, // 강수량
+    public humidity: number, // 습도
+    public rain: number, // 강수형태
+    public windDirection: string, // 풍향
+    public windSpeed: number, // 풍속
   ) {}
 }
+
+export type Weathers = {[day: string]: {[time: string]: Weather}};
 
 export class WeatherReport {
   constructor(
-    public currentWeather: Weather,
-    public hourlyWeather: HourlyWeather[],
+    public currentWeather: CurWeather,
+    public hourlyWeather: Weathers,
   ) {}
 }
