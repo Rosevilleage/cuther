62b4a7b28cbc608d4bb20220d129c9568552dd72
rosevilleage <chanhee.frontend@gmail.com>
2025-03-11 21:52:13 +0900

    install tanstack query

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import NetInfo from '@react-native-community/netinfo';
+import {onlineManager} from '@tanstack/react-query';
+
+onlineManager.setEventListener(setOnline => {
+  return NetInfo.addEventListener(state => {
+    setOnline(!!state.isConnected);
+  });
+});
+
 AppRegistry.registerComponent(appName, () => App);
