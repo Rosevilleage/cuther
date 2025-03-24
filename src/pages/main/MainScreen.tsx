94bb6ce32cf96490a7b7a4b74e113bc414c68790
rosevilleage <chanhee.frontend@gmail.com>
2025-03-24 20:53:54 +0900

    특보, 예비 특보 적용

--- src/pages/main/MainScreen.tsx
+++ src/pages/main/MainScreen.tsx
@@ -4,20 +4,32 @@ import {ScrollView, useWindowDimensions, View} from 'react-native';
 import CurrentDisplay from '../../widgets/CurrentDisplay';
 import WeatherList from '../../widgets/WeatherList';
 import TodayHourlyWeathers from '../../widgets/TodayHourlyWeathers';
-import SimpleReportView from '../../widgets/SimpleReportView';
+
 import {useQuery} from '@tanstack/react-query';
 import {
+  dailyConditionQueryOption,
+  dailyTemperatureQueryOption,
   hourlyQueryOption,
   nowWeatherQueryOption,
   sunRiseSetQueryOption,
 } from '../../features/weather/lib/weatherQueries';
-import {useGeoLocation} from '../../features/geoLocation/model/geoLocationStore';
+import {
+  MidTaRegId,
+  RegId,
+  useGeoLocation,
+} from '../../features/geoLocation/model/geoLocationStore';
 import dayjs from 'dayjs';
 import {roundToNearestBaseTime} from '../../features/weather/lib/weatherUtil';
 import {BaseDate, BaseTime} from '../../features/weather/model/weatherDTO';
+import SimpleReportView from '../../widgets/SimpleReportView';
+import {
+  preReportQueryOption,
+  specialReportQueryOption,
+} from '../../features/specialReport/lib/specialReportQueries';
+import {DailyWeathers} from '../../entitites/Weather';
 
 export default function MainScreen() {
-  const {nx, ny, lat, lng} = useGeoLocation();
+  const {nx, ny, lat, lng, regId, midTaRegId, stnId} = useGeoLocation();
   const base_date = dayjs().format('YYYYMMDD') as BaseDate;
   const base_time = dayjs().format('HHmm') as BaseTime;
   const hourlyDate = roundToNearestBaseTime(base_date, base_time);
@@ -30,9 +42,9 @@ export default function MainScreen() {
     // isLoading: isNowWeatherLoading,
   } = useQuery(nowWeatherQueryOption({base_date, base_time, nx, ny}));
   const {
-    data: dailyWeathers,
-    // isError: isDailyWeatherError,
-    // isLoading: isDailyWeatherLoisLoading,
+    data: hourlyWeathers,
+    // isError: isHourlyWeatherError,
+    // isLoading: isHourlyWeatherLoisLoading,
   } = useQuery(
     hourlyQueryOption({
       base_date: hourlyDate.baseDate,
@@ -46,7 +58,34 @@ export default function MainScreen() {
     // isError: isRiseSetError,
     // isLoading: isRiseSetLoisLoading,
   } = useQuery(sunRiseSetQueryOption(base_date, lat, lng));
+  const tmFc =
+    +base_time < 610
+      ? `${dayjs(base_date).subtract(1, 'days').format('YYYYMMDD')}1800`
+      : +base_time < 1810
+      ? `${base_date}0600`
+      : `${base_date}1800`;
+  const {
+    data: dailyConditions,
+    // isError: dailyWeatherIsError,
+    // isLoading: dailyWeatherIsLoading,
+  } = useQuery(dailyConditionQueryOption(regId as RegId, tmFc));
+  const {data: dailyTemperature} = useQuery(
+    dailyTemperatureQueryOption(midTaRegId as MidTaRegId, tmFc),
+  );
+  const dailyWeathers: DailyWeathers[] | undefined =
+    dailyConditions && dailyTemperature
+      ? dailyConditions.map((conditions, i) => {
+          return {
+            ...conditions,
+            ...dailyTemperature[i],
+          };
+        })
+      : undefined;
 
+  const {data: sepcialReports} = useQuery(
+    specialReportQueryOption(stnId as number),
+  );
+  const {data: preReports} = useQuery(preReportQueryOption(stnId as number));
   return (
     <ScrollView style={{width, height}}>
       <View style={{flex: 1, padding: 10, gap: 15}}>
@@ -56,25 +95,34 @@ export default function MainScreen() {
             currentWeather={currentWeather}
             sunRiseSet={sunRiseSet}
             condition={
-              dailyWeathers && Object.keys(dailyWeathers)[0]
-                ? dailyWeathers[Object.keys(dailyWeathers)[0]].weathers[0]
+              hourlyWeathers && Object.keys(hourlyWeathers)[0]
+                ? hourlyWeathers[Object.keys(hourlyWeathers)[0]].weathers[0]
                     .condition
                 : 1
             }
           />
         )}
         {/* 시간별 날씨 */}
-        {dailyWeathers && sunRiseSet && (
+        {hourlyWeathers && sunRiseSet && (
           <TodayHourlyWeathers
-            weathers={dailyWeathers}
+            weathers={hourlyWeathers}
             sunRiseSet={sunRiseSet}
           />
         )}
         {/* 특보 */}
-        <SimpleReportView />
+        {sepcialReports && preReports && (
+          <SimpleReportView
+            specialReports={sepcialReports}
+            preReports={preReports}
+          />
+        )}
         {/* 단기 */}
-        {dailyWeathers && sunRiseSet && (
-          <WeatherList weathers={dailyWeathers} sunRiseSet={sunRiseSet} />
+        {hourlyWeathers && sunRiseSet && dailyWeathers && (
+          <WeatherList
+            hourlyWeathers={hourlyWeathers}
+            dailyWeathers={dailyWeathers}
+            sunRiseSet={sunRiseSet}
+          />
         )}
       </View>
     </ScrollView>
