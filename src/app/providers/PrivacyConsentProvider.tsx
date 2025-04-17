8d5c4ab2421afb8160e24fcb2bd16f3e8fa01cc5
rosevilleage <chanhee.frontend@gmail.com>
2025-04-17 17:14:06 +0900

    개인정보 동의 여부 확인 및 분기 provider 생성

---
+++ src/app/providers/PrivacyConsentProvider.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react';
+import privacyConsentStore from '../store/privacyConsentStore';
+import WelcomeScreen from '../../pages/WelcomeScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+AsyncStorage.removeItem('privacyPolicyAgreed');
+
+export const PrivacyConsentProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const {isPrivacyPolicyAgreed, setPrivacyPolicyAgreed} = privacyConsentStore();
+  useEffect(() => {
+    async function checkAgreed() {
+      const isAgreed = await AsyncStorage.getItem('privacyPolicyAgreed');
+      setPrivacyPolicyAgreed(isAgreed === 'true');
+    }
+    checkAgreed();
+  }, [setPrivacyPolicyAgreed]);
+
+  if (!isPrivacyPolicyAgreed) {
+    return <WelcomeScreen />;
+  }
+  return <>{children}</>;
+};
