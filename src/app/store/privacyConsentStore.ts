f374b406e59e28747284f5e77f7a477d2bae1b0e
rosevilleage <chanhee.frontend@gmail.com>
2025-04-17 17:13:32 +0900

    개인정보 동의 상태 관리 스토어 추가

---
+++ src/app/store/privacyConsentStore.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {create} from 'zustand';
+
+interface PrivacyConsentStore {
+  isPrivacyPolicyAgreed: boolean;
+  setPrivacyPolicyAgreed: (isAgreed: boolean) => Promise<void>;
+}
+
+const privacyConsentStore = create<PrivacyConsentStore>(set => ({
+  isPrivacyPolicyAgreed: false,
+  setPrivacyPolicyAgreed: async (isAgreed: boolean) => {
+    await AsyncStorage.setItem('privacyPolicyAgreed', isAgreed.toString());
+    set({isPrivacyPolicyAgreed: isAgreed});
+  },
+}));
+
+export default privacyConsentStore;
