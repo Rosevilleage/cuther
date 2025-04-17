5c64b6bc4d4dfad818751fc1490a553b36683d33
rosevilleage <chanhee.frontend@gmail.com>
2025-04-17 18:24:14 +0900

    networkProvider export default로 변경

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {NetworkProvider} from './src/app/providers/NetworkProvider';
+import NetworkProvider from './src/app/providers/NetworkProvider';
 import Navigation from './src/app/routes/navigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import LocationProvider from './src/app/providers/LocationProvider';
